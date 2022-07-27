aefff03b4f04564844c7d2f6152ea00d942c25f1
Globox1997 <mitschke11@web.de>
2022-07-27 12:11:48 +0200

    tweaked evoker totem usage

--- src/main/java/net/additionz/mixin/EvokerEntityMixin.java
+++ src/main/java/net/additionz/mixin/EvokerEntityMixin.java
@@ -1,6 +1,9 @@
 package net.additionz.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.additionz.AdditionMain;
 import net.minecraft.entity.EntityStatuses;
@@ -10,13 +13,25 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.EvokerEntity;
 import net.minecraft.entity.mob.SpellcastingIllagerEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
 
 @Mixin(EvokerEntity.class)
 public abstract class EvokerEntityMixin extends SpellcastingIllagerEntity {
+    private boolean usedTotem = false;
 
     public EvokerEntityMixin(EntityType<? extends SpellcastingIllagerEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Inject(method = "readCustomDataFromNbt", at = @At("TAIL"))
+    private void readCustomDataFromNbtMixin(NbtCompound nbt, CallbackInfo info) {
+        this.usedTotem = nbt.getBoolean("UsedTotem");
+    }
+
+    @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
+    private void writeCustomDataToNbtMixin(NbtCompound nbt, CallbackInfo info) {
+        nbt.putBoolean("UsedTotem", this.usedTotem);
     }
 
     @Override
@@ -29,7 +44,8 @@ public abstract class EvokerEntityMixin extends SpellcastingIllagerEntity {
         if (source.isOutOfWorld()) {
             return false;
         }
-        if (AdditionMain.CONFIG.evoker_use_totem_chance > 0.001F && this.world.random.nextFloat() <= AdditionMain.CONFIG.evoker_use_totem_chance) {
+        if (!this.usedTotem && AdditionMain.CONFIG.evoker_use_totem_chance > 0.001F && this.world.random.nextFloat() <= AdditionMain.CONFIG.evoker_use_totem_chance) {
+            this.usedTotem = true;
             this.setHealth(this.getMaxHealth() * 0.5F);
             this.clearStatusEffects();
             this.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 900, 1));
