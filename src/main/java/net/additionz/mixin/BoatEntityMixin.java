f80c8515b7c81de28c8d8638d34a05787b4dbed5
Globox1997 <mitschke11@web.de>
2023-05-16 11:33:34 +0200

    added more boat nerf

--- src/main/java/net/additionz/mixin/BoatEntityMixin.java
+++ src/main/java/net/additionz/mixin/BoatEntityMixin.java
@@ -3,9 +3,11 @@ package net.additionz.mixin;
 import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.additionz.AdditionMain;
 import net.minecraft.block.BlockState;
@@ -14,11 +16,15 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.entity.vehicle.BoatEntity.Location;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 @Mixin(BoatEntity.class)
 public abstract class BoatEntityMixin extends Entity {
+
+    @Shadow
+    private float nearbySlipperiness;
 
     public BoatEntityMixin(EntityType<?> type, World world) {
         super(type, world);
@@ -35,6 +41,13 @@ public abstract class BoatEntityMixin extends Entity {
                     }
                 }
             }
+        }
+    }
+
+    @Inject(method = "checkLocation", at = @At(value = "RETURN", ordinal = 2))
+    private void checkLocationMixin(CallbackInfoReturnable<Location> info) {
+        if (AdditionMain.CONFIG.boat_fall_damage_nerf) {
+            this.nearbySlipperiness = 0.6f;
         }
     }
 
