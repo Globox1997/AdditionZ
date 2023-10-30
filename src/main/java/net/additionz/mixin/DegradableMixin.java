3c44835f53ed04d10ed79392276cb7d362e6cb07
Globox1997 <mitschke11@web.de>
2023-10-30 14:55:16 +0100

    changed oxidization feature

--- src/main/java/net/additionz/mixin/DegradableMixin.java
+++ src/main/java/net/additionz/mixin/DegradableMixin.java
@@ -1,12 +1,9 @@
 package net.additionz.mixin;
 
-import java.util.Optional;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.additionz.AdditionMain;
 import net.minecraft.block.BlockState;
@@ -20,8 +17,8 @@ import net.minecraft.util.math.random.Random;
 @Mixin(Degradable.class)
 public interface DegradableMixin {
 
-    @Inject(method = "tryDegrade", at = @At("HEAD"), cancellable = true)
-    default public void tryDegradeMixin(BlockState state2, ServerWorld world, BlockPos pos, Random random, CallbackInfo info) {
+    @ModifyConstant(method = "tickDegradation", constant = @Constant(floatValue = 0.05688889f, ordinal = 1))
+    default public float tickDegradationModifyMixin(float original, BlockState state, ServerWorld world, BlockPos pos, Random random) {
         if (AdditionMain.CONFIG.fast_oxidization) {
             boolean hasFire = false;
             boolean hasWater = false;
@@ -38,13 +35,14 @@ public interface DegradableMixin {
             }
 
             if (hasFire && hasWater && random.nextFloat() < 0.7F) {
-                this.getDegradationResult(state2).ifPresent(state -> world.setBlockState(pos, (BlockState) state));
-                info.cancel();
+                tryDegrade(state, world, pos, random);
             }
         }
+        return original;
     }
 
     @Shadow
-    public Optional<BlockState> getDegradationResult(BlockState var1);
+    default public void tryDegrade(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+    }
 
 }
